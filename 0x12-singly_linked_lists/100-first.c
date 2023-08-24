46569c155e218b8d8a1160cadeb2c69342637faf
Ossycity <oscarmbithi4@gmail.com>
2023-08-24 13:51:51 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-void custom_init(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
- * custom_init - prints a custom message before the main function is executed
+ * first - prints a sentence before the main
+ * function is executed
  */
-void custom_init(void)
+void first(void)
 {
-    printf("This is a custom initialization message.\n");
-    printf("Before the main function starts running.\n");
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
 }
