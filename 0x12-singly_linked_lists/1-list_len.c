2d7c304823957bf50794c15855684e33f2877737
Ossycity <oscarmbithi4@gmail.com>
2023-08-24 13:38:41 -0700

    len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,21 @@
+#include "lists.h"
 #include <stdlib.h>
-#include "custom_lists.h"
 
 /**
- * custom_list_length - returns the number of elements in a custom linked list
- * @head: pointer to the custom_node_t list
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- * Return: number of elements in the custom list
+ * Return: number of elements in h
  */
-size_t custom_list_length(const custom_node_t *head)
+size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+    size_t n = 0;
 
-    while (head)
+    while (h)
     {
-        count++;
-        head = head->next;
+        n++;
+        h = h->next;
     }
-
-    return (count);
+    
+    return (n);
 }
